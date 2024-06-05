32616d6cafa12a8103e22684a304fe9470ab075a
SEUNG HO JUNG <jhoho82@gmail.com>
2024-06-05 12:55:32 +0900

    initail commit test!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Store from './Store';
+import {Resturant, Address} from './model/resturant'
+import BestMenu from "./BestMenu";
 
-function App() {
-  return (
+
+let data: Resturant = {
+  name: '정금자 식당',
+  category: 'western',
+  address:{
+    city:'Paju',
+    detail: 'somewhere',
+    zipCode:109301,
+  },
+  menu:[{name:"rose pasta", price:2000, category:"PASTA"}, {name:"garlic steak", price: 3000, category:"STEAK"}]
+}
+
+const  App:React.FC = () => {
+  const [myResturant, setMyResturant] = useState< Resturant >(data)
+  const changeAddress = (address:Address) => {
+       setMyResturant({...myResturant, address:address})
+  }
+
+  const showBestMenuName = (name:string)=>{
+    return name
+  }
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Store info={myResturant} changeAddress={changeAddress} />
+      <BestMenu name="불고기피자" category="피자" showBestMenuName={showBestMenuName} />
+
     </div>
   );
 }
